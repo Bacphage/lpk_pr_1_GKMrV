144ad1ffbffaccd7b1fca3ec550fb9db6e9b3879
Bacphage <165451570+Bacphage@users.noreply.github.com>
2024-05-15 05:47:00 +0700

    Update 4_Kalkulator Berat Ekuivalen (BE).py

--- pages/4_Kalkulator Berat Ekuivalen (BE).py
+++ pages/4_Kalkulator Berat Ekuivalen (BE).py
@@ -18,7 +18,7 @@ def custom_calculator():
 
     if st.button("Hitung"):
         # Memastikan Valensi tidak boleh 0 untuk menghindari pembagian dengan nol
-        elif valensi == 0:
+        if valensi == 0:
             st.error("Valensi tidak boleh nol!")
         else:
             # Menghitung hasil pembagian Mr / Valensi
