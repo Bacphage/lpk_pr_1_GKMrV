958d10652b3b1c7b9b9a46082025b266cd23d90d
Bacphage <165451570+Bacphage@users.noreply.github.com>
2024-05-16 11:32:09 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,22 @@
 import streamlit as st 
+from PIL import Image
+img=Image.open("pages/Politeknik_Akademi_Kimia_Analisis_Bogor.png")
 
 st.set_page_config(
     page_title="Generator & Kalkulator Massa Molekul Relatif & Valensi",
     page_icon="🗜️"
 )
 st.markdown("<h1 style='text-align: center; color: white;'>Generator & Kalkulator Massa Molekul Relatif & Valensi</h1>", unsafe_allow_html=True)
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write("")
+
+with col2:
+    st.image(img, width=200)
+
+with col3:
+    st.write("")
+
 st.header("")
 st.markdown("")
 st.markdown("Silahkan pilih opsi menu dibawah ini")
@@ -14,3 +26,4 @@ st.page_link("pages/1_Pengertian dan Informasi.py", label="Pengertian dan Inform
 st.page_link("pages/2_Generator Massa Molekul Relatif (Mr).py", label="Generator Massa Molekul Relatif (Mr)", icon="🧪")
 st.page_link("pages/3_Generator Valensi Asam dan Basa (α).py", label="Generator Valensi Asam dan Basa (α)", icon="⚗️")
 st.page_link("pages/4_Kalkulator Berat Ekuivalen (BE).py", label="Kalkulator Berat Ekuivalen (BE)", icon="🧮")
+st.page_link("pages/5_Kritik Saran dan Credits.py", label="Kritik, Saran, dan _Credits_", icon="📬")
