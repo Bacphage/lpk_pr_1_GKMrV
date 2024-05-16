ab6e9812d7f63b9d3440d004ee03b1222d761db5
Bacphage <165451570+Bacphage@users.noreply.github.com>
2024-05-16 11:32:33 +0700

    Add files via upload

---
+++ pages/5_Kritik Saran dan Credits.py
@@ -0,0 +1,38 @@
+from cgitb import grey
+import streamlit as st
+
+st.set_page_config(
+    page_title="Kririk, Saran, dan Credits",
+    page_icon="📬"
+    )
+
+st.page_link("Main.py", label="Main Menu", icon="🗜️")
+st.title(":green[Kritik, Saran], dan :orange[_Credits_]")
+st.header(":green[Kritik dan Saran]", divider='grey')
+st.markdown('''Silahkan ketuk 'link' dibawah untuk memberikan kritik dan saran mengenai _website_ ini
+            agar nantinya bisa berkembang lebih baik lagi 👍 ''')
+st.markdown('')
+st.write("ketuk ➡️[link](https://forms.gle/bpYe9ZaoboMUYfw2A) ⬅️")
+st.markdown('')
+st.markdown('')
+st.markdown('')
+st.markdown('')
+st.markdown('')
+st.header(":orange[_Credits_]", divider="grey")
+st.markdown("website ini dibuat oleh:")
+st.markdown(':green[Deril Januar Pratama]')
+st.markdown(':green[Haikal Ramadhan Akbar]')
+st.markdown(':green[Muhamad Reza Syah Fahlevi]')
+st.markdown(':green[Muhammad Daffa Arraiyan]')
+st.markdown('Valdy Syahri')
+st.markdown('')
+st.markdown('')
+st.markdown(':green[dan dibantu oleh pihak lainnya yang tidak disebutkan]')
+st.markdown('')
+st.markdown('')
+st.markdown('')
+st.markdown('')
+st.markdown('_bagian dari projek Praktik Logika dan Pemograman Komputer_')
+st.markdown(':violet[Dosen Penanggung Jawab]')
+st.markdown(':violet[Dewi Pujo Ningsih, M.Si]')
+st.markdown('_Powered by Politeknik AKA BOGOR_')
